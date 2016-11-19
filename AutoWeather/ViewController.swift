d23401ee6dfd02465bf02d552023d1e3fa6f19ec
Shady Gabal <shady@nyu.edu>
2016-11-18 19:42:04 -0500

    Initial Commit

---
+++ AutoWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoWeather
+//
+//  Created by Shady Gabal on 11/18/16.
+//  Copyright © 2016 Shady Gabal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
